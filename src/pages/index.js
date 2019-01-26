4d65c67166866ad4fff903cfaa6496917fa8cbcf
ivrunner <richyentam@gmail.com>
2019-01-26 00:15:35 -0500

    ttlogo added

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import catAndHumanIllustration from "../images/cat-and-human-illustration.svg";
+import ttLogo from "../images/tt-logo.svg";
 
 const IndexPage = () => (
   <Layout>
@@ -10,25 +11,10 @@ const IndexPage = () => (
 
     <div className="text-center">
       <img
-        src={catAndHumanIllustration}
+        src={ttLogo}
         className="block mx-auto w-1/2"
         alt="Cat and human sitting on a couch"
       />
-
-      <h2 className="bg-yellow inline-block my-8 p-3">
-        Hey there! Welcome to your first Gatsby site.
-      </h2>
-
-      <p className="leading-loose">
-        This is a barebones starter for Gatsby styled using{" "}
-        <a
-          href="https://tailwindcss.com/"
-          className="font-bold no-underline text-grey-darkest"
-        >
-          Tailwind
-        </a>
-        , a utility-first CSS framework.
-      </p>
     </div>
   </Layout>
 );
